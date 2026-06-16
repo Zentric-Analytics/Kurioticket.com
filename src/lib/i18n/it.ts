1f802e831a967019c9f35d25261ffece5ce44d2e
zentricresearch <admin@zentricresearch.com>
2026-06-16 02:56:20 -0700

    Fix country currency selector i18n

--- src/lib/i18n/it.ts
+++ src/lib/i18n/it.ts
@@ -47,6 +47,7 @@ export const translations: TranslationDictionary = {
   "countryCurrency.country.IN": "India",
   "countryCurrency.country.BR": "Brasile",
   "countryCurrency.country.MX": "Messico",
+  "countryCurrency.country.SG": "Singapore",
   explore: "Esplora",
   login: "Accedi",
   signIn: "Accedi",
