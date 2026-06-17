d203894c76b70ce17b780cd491e823fa9264036c
zentriclab1 <developer1@zentricresearch.com>
2026-06-17 13:29:29 +0200

    Polish account hub layout

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -3,14 +3,14 @@ import { AppHeader } from "@/components/layout/AppHeader";
 import { Footer } from "@/components/layout/Footer";
 
 export const metadata = {
-  title: "My account",
+  title: "My Account",
 };
 
 export default function AccountPage() {
   return (
     <>
       <AppHeader />
-      <main className="flex-1 bg-white pb-10 pt-6 sm:pt-8 lg:pt-28">
+      <main className="flex-1 bg-white pb-14 pt-5 sm:pt-7 lg:pb-18 lg:pt-14">
         <div className="page-shell min-w-0">
           <AccountMenuPage />
         </div>
