c21cc68d304722e258849e30dc992e1ce8344e36
zentriclab1 <developer1@zentricresearch.com>
2026-07-04 17:59:38 +0100

    Point legacy customization route to new preferences page

--- src/app/dashboard/preferences/customization/page.tsx
+++ src/app/dashboard/preferences/customization/page.tsx
@@ -1,25 +1,5 @@
-import { cookies } from "next/headers";
-import { AccountPreferencesHeader } from "../AccountPreferencesHeader";
-import { Footer } from "@/components/layout/Footer";
-import { CustomizationPreferencesContent } from "./CustomizationPreferencesContent";
-import { getTranslations } from "@/lib/i18n";
-import { LOCALE_COOKIE_KEY } from "@/lib/preferences/preferences";
-
-export async function generateMetadata() {
-  const cookieStore = await cookies();
-  const t = getTranslations(cookieStore.get(LOCALE_COOKIE_KEY)?.value);
-
-  return { title: t["accountDashboard.preferences.customization.title"] };
-}
+import { redirect } from "next/navigation";
 
 export default function CustomizationPreferencesPage() {
-  return (
-    <>
-      <div className="[&>header]:!border-b-0 [&>header]:!shadow-none">
-        <AccountPreferencesHeader />
-      </div>
-      <CustomizationPreferencesContent />
-      <Footer />
-    </>
-  );
+  redirect("/dashboard/preferences");
 }
