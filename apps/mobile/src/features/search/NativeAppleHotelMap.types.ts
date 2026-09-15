a593b9792c9760bed107961daa5f4d4509a60b01
Oluwadunbarin Olayinka <oluwadunbarin.olayinka@zentricanalytics.com>
2026-09-15 06:19:22 -0700

    Allow shared Apple map legal insets

--- apps/mobile/src/features/search/NativeAppleHotelMap.types.ts
+++ apps/mobile/src/features/search/NativeAppleHotelMap.types.ts
@@ -1,1 +1,14 @@
-export type NativeAppleHotelMapProps = { latitude: number; longitude: number; hotelName: string; interactive?: boolean };
+export type NativeAppleHotelMapInsets = {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+};
+
+export type NativeAppleHotelMapProps = {
+  latitude: number;
+  longitude: number;
+  hotelName: string;
+  interactive?: boolean;
+  legalLabelInsets?: NativeAppleHotelMapInsets;
+};
