aefa8ad6c074294846ef35975dfd3126ab93af58
Ogunlade Bisola <bisola.ogunlade@zentricanalytics.com>
2026-09-23 14:20:42 +0100

    Add Hotel mobile iOS browser detection

---
+++ src/lib/hotels/iosHotelMobileWeb.ts
@@ -0,0 +1,7 @@
+export function isIosHotelMobileWeb() {
+  if (typeof navigator === "undefined") return false;
+  const userAgent = navigator.userAgent || "";
+  const platform = navigator.platform || "";
+  return /iPad|iPhone|iPod/i.test(userAgent)
+    || (platform === "MacIntel" && navigator.maxTouchPoints > 1);
+}
